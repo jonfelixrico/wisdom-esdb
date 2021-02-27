4636d97a6e5b5f7243eb98d7be302ad4d594e4b5
Felix <jonfelixrico@gmail.com>
2021-02-28 00:20:54 +0800

    Add drop quote method in the quote repo

--- src/models/repository/quote-repository.abstract.ts
+++ src/models/repository/quote-repository.abstract.ts
@@ -1,13 +1,18 @@
-import { IQuoteBody } from '@models/quote.interface'
+import { IQuote } from '@models/quote.interface'
 import { IPendingQuote } from '@models/pending-quote.interface'
 import { IApprovedQuote } from '@models/approved-quote.interface'
+import { IDroppedQuote } from '@models/dropped-quote.interface'
 
 export abstract class QuoteRepository {
   /**
    * Submit a quote for approval.
    * @param quote
    */
-  abstract submitQuote(quote: IQuoteBody): Promise<IPendingQuote>
+  abstract submitQuote(quote: IQuote): Promise<IPendingQuote>
+
+  abstract getQuote(
+    quoteId: string,
+  ): Promise<IPendingQuote | IApprovedQuote | IDroppedQuote>
 
   /**
    * Retrieve an approved quote from the repository.
@@ -21,4 +26,6 @@ export abstract class QuoteRepository {
    * @param quoteId The id of a pending quote.
    */
   abstract approveQuote(quoteId: string): Promise<IApprovedQuote>
+
+  abstract dropQuote(quoteId: string, cause: string): Promise<IDroppedQuote>
 }
