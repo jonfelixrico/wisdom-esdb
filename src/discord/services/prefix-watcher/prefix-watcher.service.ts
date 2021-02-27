e83c7c6791abea35d294e7dd58e155563f7e9ef6
Felix <jonfelixrico@gmail.com>
2021-02-28 02:39:53 +0800

    Create util for matching prefixes

--- src/discord/services/prefix-watcher/prefix-watcher.service.ts
+++ src/discord/services/prefix-watcher/prefix-watcher.service.ts
@@ -2,46 +2,36 @@ import { Injectable } from '@nestjs/common'
 import { PrefixRepository } from '@discord/models/repositories/prefix-repository.abstract'
 import { Client, Message } from 'discord.js'
 import { Subject } from 'rxjs'
+import { matchPrefix } from '@discord/utils/command-utils.util'
 
 export interface ICommandBusPayload {
   command: string
   message: Message
 }
 
-const PREFIX_REGEXP = /^(\S+)\s+(.+?)\s*$/
-
 @Injectable()
 export class PrefixWatcherService {
   private emitter = new Subject<ICommandBusPayload>()
 
   constructor(private client: Client, private prefixRepo: PrefixRepository) {
-    this.initWatcher()
+    this.onInit()
   }
 
   get commandBus() {
     return this.emitter.asObservable()
   }
 
-  initWatcher() {
+  onInit() {
     this.client.on('message', async (message) => {
-      if (!PREFIX_REGEXP.test(message.content)) {
-        return
-      }
-
-      /*
-       * Check if the content starts with the guild's assigned prefix for the bot.
-       * If it matches, extract the command portion (e.g. submit <...params>, receive) and
-       * dispatch it in the command bus.
-       */
-
-      const [prefix, command] = PREFIX_REGEXP.exec(message.content).slice(1)
       const guildPrefix = await this.prefixRepo.getPrefix(message.guild.id)
-      if (prefix !== guildPrefix) {
+
+      const extractedCommand = matchPrefix(guildPrefix, message.content)
+      if (extractedCommand === false) {
         return
       }
 
       this.emitter.next({
-        command,
+        command: extractedCommand,
         message,
       })
     })
---
+++ src/discord/utils/command-utils.util.ts
@@ -0,0 +1,17 @@
+const PREFIX_MATCHER_REGEXP = /^(\S+)\s+(\S+?)\s*$/
+
+export function matchPrefix(prefix: string, stringToMatch: string) {
+  if (!PREFIX_MATCHER_REGEXP.test(stringToMatch)) {
+    return false
+  }
+
+  const [foundPrefix, content] = PREFIX_MATCHER_REGEXP.exec(
+    stringToMatch,
+  ).slice(1)
+
+  if (foundPrefix !== prefix) {
+    return false
+  }
+
+  return content
+}
