fb373b5c81c329da829b833ab754fcd2c529f991
Felix <jonfelixrico@gmail.com>
2021-02-27 22:53:25 +0800

    Generate the `interactors` module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common'
+import { InteractorsModule } from './interactors/interactors.module'
 
 @Module({
-  imports: [],
+  imports: [InteractorsModule],
   controllers: [],
   providers: [],
 })
