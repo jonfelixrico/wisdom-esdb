694ea0a0215a3b7d4290e89c5adf4db3b2162526
Felix <jonfelixrico@gmail.com>
2021-02-27 23:06:47 +0800

    Disable some tslint rules temporarily

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,8 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+
+    // TODO remove this after initial coding
+    '@typescript-eslint/no-empty-interface': 'off',
   },
-};
+}
