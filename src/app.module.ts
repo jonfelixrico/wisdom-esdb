dcb83adf2eb4cdb5ab4b3e659b006685cada7440
Felix <jonfelixrico@gmail.com>
2021-02-28 16:45:30 +0800

    Generate the repositories module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { InteractorsModule } from './interactors/interactors.module'
 import { DiscordModule } from './discord/discord.module'
+import { RepositoriesModule } from './repositories/repositories.module'
 
 @Module({
-  imports: [InteractorsModule, DiscordModule],
+  imports: [InteractorsModule, DiscordModule, RepositoriesModule],
   controllers: [],
   providers: [],
 })
