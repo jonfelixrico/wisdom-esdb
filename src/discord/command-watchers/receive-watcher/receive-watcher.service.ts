b8a4ac7a73a9c44f6e087d45564c8df4dff21bb6
Felix <jonfelixrico@gmail.com>
2021-02-28 16:41:53 +0800

    Push initial code for the receive watcher

--- src/discord/command-watchers/receive-watcher/receive-watcher.service.ts
+++ src/discord/command-watchers/receive-watcher/receive-watcher.service.ts
@@ -1,4 +1,44 @@
+import { PrefixWatcherService } from '@discord/services/prefix-watcher/prefix-watcher.service'
+import { ReceiveInteractorService } from '@interactors/receive-interactor/receive-interactor.service'
 import { Injectable } from '@nestjs/common'
+import { filter } from 'rxjs/operators'
+import { ICommandBusPayload } from '@discord/services/prefix-watcher/prefix-watcher.service'
+
+const RECEIVE_COMMAND_PREFIX = 'receive'
 
 @Injectable()
-export class ReceiveWatcherService {}
+export class ReceiveWatcherService {
+  constructor(
+    private receiveInteractor: ReceiveInteractorService,
+    private watcher: PrefixWatcherService,
+  ) {
+    this.listenToCommandBus()
+  }
+
+  async handler({ message }: ICommandBusPayload) {
+    await message.react('🌬️')
+    const response = await message.channel.send('🤔')
+
+    const received = await this.receiveInteractor.receiveQuote({
+      channel: message.channel.id,
+      guild: message.guild.id,
+      message: response.id,
+
+      receiveBy: message.author.id,
+      receiveDt: new Date(),
+    })
+
+    await response.edit(JSON.stringify(received))
+  }
+
+  get commandBus$() {
+    return this.watcher.commandBus.pipe(
+      filter(({ command }) => command === RECEIVE_COMMAND_PREFIX),
+    )
+  }
+
+  listenToCommandBus() {
+    const { commandBus$: commandCall$, handler } = this
+    commandCall$.subscribe(handler.bind(this))
+  }
+}
