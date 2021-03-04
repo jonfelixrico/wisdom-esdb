ce9e3417bca16301b21c22fe7669526d16e4cd45
Felix <jonfelixrico@gmail.com>
2021-03-04 23:00:44 +0800

    Integrate the PrefixRepository abstract into PrefixRepositoryService

--- src/repositories/prefix-repository/prefix-repository.service.ts
+++ src/repositories/prefix-repository/prefix-repository.service.ts
@@ -1,4 +1,10 @@
 import { Injectable } from '@nestjs/common'
+import { PrefixRepository } from '@repositories/models/prefix-repository.abstract'
 
 @Injectable()
-export class PrefixRepositoryService {}
+export class PrefixRepositoryService extends PrefixRepository {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async getPrefix(guildId: string): Promise<string> {
+    return '!wisdom'
+  }
+}
