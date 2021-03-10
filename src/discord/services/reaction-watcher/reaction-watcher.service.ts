8c2be382e93ba61d3f6ab88a23ff0fc967eeb329
Felix <jonfelixrico@gmail.com>
2021-03-10 20:08:22 +0800

    Apply corrections and comments to ReactionWatcherService

--- src/discord/services/reaction-watcher/reaction-watcher.service.ts
+++ src/discord/services/reaction-watcher/reaction-watcher.service.ts
@@ -30,8 +30,12 @@ export class ReactionWatcherService {
   async watch(message: Message, pendingQuote: IPendingQuote) {
     const { expireDt, id, guild } = pendingQuote
 
-    const millisToExpireDt = Date.now() - expireDt.getTime()
+    const millisToExpireDt = expireDt.getTime() - Date.now()
 
+    /*
+     * A negative value means that the current date has passed the expiration date.
+     * No need to watch for reactions if the quote/message to watch has already expired.
+     */
     if (millisToExpireDt <= 0) {
       this.emit(pendingQuote.id, 'EXPIRED')
       return
@@ -41,24 +45,43 @@ export class ReactionWatcherService {
     const requiredCount = await this.guildRepo.getApprovalRequiredCount(guild)
 
     const { clientId } = this
+
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const filterFn: ReactionFilter = (reaction, user) =>
-      reaction.emoji.name === approvalEmoji && user.id !== clientId
+      reaction.emoji.name === approvalEmoji &&
+      /*
+       * This is to not count the first reaction made by the bot on the message.
+       * The first reaction by the bot was to make approval reactions easier for the users.
+       */
+      user.id !== clientId
 
     const expire$ = timer(millisToExpireDt).pipe(
       mapTo<number, CollectionStatus>('EXPIRED'),
     )
+
     const { observable, stop } = watchReactions(message, filterFn)
 
     const reactions$ = observable.pipe(
+      /*
+       * These two operations below will make the observable only emit if the required reactions
+       * for the message has been reached.
+       */
       filter((emitted) => {
         const pair = emitted.find(([emoji]) => emoji.name === approvalEmoji)
         return pair && pair[1] >= requiredCount
       }),
-      mapTo<ReactionPair[], CollectionStatus>('COMPLETED'),
       take(1),
+
+      mapTo<ReactionPair[], CollectionStatus>('COMPLETED'),
     )
 
+    /*
+     * If expire$ emits first, then that means that the submission has expired.
+     * If reactions$ emits first, then that menas that the submission has garnered enough votes
+     * for approval.
+     *
+     * Watching for reactions will stop once one of those observables emit.
+     */
     race(expire$, reactions$)
       .pipe(finalize(() => stop()))
       .subscribe((status) => this.emit(id, status))
