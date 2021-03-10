9d5332f7af1c3b3a63cf3aec42070219a3ae445c
Felix <jonfelixrico@gmail.com>
2021-03-10 19:34:44 +0800

    Modify the util for watching reactions to include more emoji information

--- src/discord/utils/discord-reaction.util.ts
+++ src/discord/utils/discord-reaction.util.ts
@@ -1,28 +1,56 @@
-import { Message, User, MessageReaction } from 'discord.js'
+import { Message, User, MessageReaction, Emoji, Snowflake } from 'discord.js'
 import { chain, clone } from 'lodash'
 import { BehaviorSubject } from 'rxjs'
 
 export type ReactionFilter = (reaction: MessageReaction, user: User) => boolean
 export type ReactionCountMap = { [emojiId: string]: number }
 
-function updateCountFactory(subject: BehaviorSubject<ReactionCountMap>) {
-  return (emojiId: string, valueChange = 1) => {
-    const valueClone = clone(subject.value)
+export interface IEmojiDetails {
+  id?: Snowflake
+  name: string
+  identifier: string
+}
 
-    if (valueClone[emojiId] === undefined) {
-      valueClone[emojiId] = 0
+export type ReactionPair = [IEmojiDetails, number]
+type ReactionPairMap = { [emojiId: string]: ReactionPair }
+
+function getKey({ name, identifier }: IEmojiDetails) {
+  return [name, identifier].join('/')
+}
+
+function extractEmojiDetails({ id, name, identifier }: Emoji): IEmojiDetails {
+  return { id, name, identifier }
+}
+
+function convertToArray(map: ReactionPairMap): ReactionPair[] {
+  return chain(map)
+    .values()
+    .sortBy(([emoji]) => getKey(emoji))
+    .map((pair) => clone(pair))
+    .value()
+}
+
+function updateCountFactory(
+  subject: BehaviorSubject<ReactionPair[]>,
+  map: ReactionPairMap,
+) {
+  return (emoji: Emoji, valueChange = 1) => {
+    const key = getKey(emoji)
+
+    if (map[key] === undefined) {
+      map[key] = [extractEmojiDetails(emoji), 0]
     }
 
-    valueClone[emojiId] += valueChange
+    map[key][1] += valueChange
 
-    subject.next(valueClone)
+    subject.next(convertToArray(map))
   }
 }
 
-function getInitialReactionCountMap(
+function getInitialReactions(
   message: Message,
   filterFn: ReactionFilter,
-): ReactionCountMap {
+): ReactionPairMap {
   const expandedReactions = message.reactions.cache
     .array()
     .map<[MessageReaction, User][]>((reaction) =>
@@ -32,8 +60,14 @@ function getInitialReactionCountMap(
     .filter(([reaction, user]) => filterFn(reaction, user))
 
   return chain(expandedReactions)
-    .groupBy(([reaction]) => reaction.emoji.id)
-    .mapValues((arrOfExpandedReactions) => arrOfExpandedReactions.length)
+    .groupBy(([reaction]) => getKey(reaction.emoji))
+    .mapValues<ReactionPair>((arrOfExpandedReactions) => {
+      const [reaction] = arrOfExpandedReactions[0]
+      return [
+        extractEmojiDetails(reaction.emoji),
+        arrOfExpandedReactions.length,
+      ]
+    })
     .value()
 }
 
@@ -41,20 +75,22 @@ export function watchReactions(
   message: Message,
   filterFn: ReactionFilter = () => true,
 ) {
-  const subject = new BehaviorSubject<ReactionCountMap>(
-    getInitialReactionCountMap(message, filterFn),
+  const reactionMap = getInitialReactions(message, filterFn)
+
+  const subject = new BehaviorSubject<ReactionPair[]>(
+    convertToArray(reactionMap),
   )
 
-  const updateCount = updateCountFactory(subject)
+  const updateCount = updateCountFactory(subject, reactionMap)
 
   const collector = message.createReactionCollector(filterFn)
 
   collector.on('collect', ({ emoji }) => {
-    updateCount(emoji.id)
+    updateCount(emoji)
   })
 
   collector.on('remove', ({ emoji }) => {
-    updateCount(emoji.id, -1)
+    updateCount(emoji, -1)
   })
 
   collector.on('end', () => {
